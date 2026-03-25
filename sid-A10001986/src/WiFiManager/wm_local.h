f1c75096d82e26823bd6be39bb8f11f0f3803056
A10001986 <76924199+realA10001986@users.noreply.github.com>
2026-03-25 21:13:25 +0100

    WM patches for 1.71

--- sid-A10001986/src/WiFiManager/wm_local.h
+++ sid-A10001986/src/WiFiManager/wm_local.h
@@ -17,7 +17,14 @@
 //#define _A10001986_V_DBG
 
 #define WM_MDNS
+
 #define WM_PARAM2
+
+#ifdef WM_PARAM2
+#define WM_PARAM2_CAPTION "HA/MQTT Settings"
+#define WM_PARAM2_TITLE "HA/MQTT Settings"
+#endif
+
 // #define WM_AP_STATIC_IP
 // #define WM_APCALLBACK
 // #define WM_PRECONNECTCB
@@ -25,10 +32,5 @@
 // #define WM_EVENTCB
 // #define WM_ADDLGETTERS
 // #define WM_ADDLSETTERS
-
-#ifdef WM_PARAM2
-#define WM_PARAM2_CAPTION "HA/MQTT Settings"
-#define WM_PARAM2_TITLE "HA/MQTT Settings"
-#endif
 
 #endif
